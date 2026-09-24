eaa2a416520b3df664c19578167eba1dafb36de7
AadilKhanPathan <aadilkhanpathan937128@gmail.com>
2026-09-24 23:20:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "console.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
     ],
   },
   reactCompiler: true,
